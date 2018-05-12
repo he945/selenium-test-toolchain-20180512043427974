858bc8eacda63ed965d3e052caf3ee4f147bf77e
jumpshot 99 <jumpshot99@gmail.com>
2018-05-12 07:47:31 +0000

    added file, removed file

---
+++ tests/test_chrome.py
@@ -0,0 +1,31 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import selenium.webdriver.support.ui as ui
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException
+
+class ChromeTest(unittest.TestCase):
+
+    def setUp(self):
+    	options = webdriver.ChromeOptions()
+		options.binary_location = "/usr/bin/google-chrome-beta"
+		options.add_experimental_option( "prefs", {'plugin.state.flash': 1, "profile.default_content_setting_values.plugins":1, "profile.content_settings.plugin_whitelist.adobe-flash-player": 1, "profile.content_settings.exceptions.plugins.*,*.per_resource.adobe-flash-player": 1, "PluginsAllowedForUrls": "http://abc.go.com/watch-live"})
+		options.add_argument('--always-authorize-plugins')
+		options.add_argument("--no-sandbox")
+		options.add_argument('--allow-outdated-plugins')
+		options.add_argument('--ppapi-flash-path=/usr/lib/pepperflashplugin-nonfree/libpepflashplayer.so')
+        self.driver = webdriver.Chrome(chrome_options=options)
+
+    def test_page(self):
+        self.driver.get("https://www.luckyseat.com/hamilton-ny/")
+        new_elem = self.driver.find_element_by_tag_name("body");
+        assert "RODGERS " in new_elem.text
+
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
+
