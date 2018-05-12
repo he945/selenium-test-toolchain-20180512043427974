f1a239b6e9fd4579487be4696d042c390005bada
jumpshot 99 <jumpshot99@gmail.com>
2018-05-12 08:30:25 +0000

    updated

--- tests/test_chrome.py
+++ tests/test_chrome.py
@@ -8,13 +8,13 @@ from selenium.common.exceptions import NoSuchElementException
 class ChromeTest(unittest.TestCase):
 
     def setUp(self):
-    	options = webdriver.ChromeOptions()
-		options.binary_location = "/usr/bin/google-chrome-beta"
-		options.add_experimental_option( "prefs", {'plugin.state.flash': 1, "profile.default_content_setting_values.plugins":1, "profile.content_settings.plugin_whitelist.adobe-flash-player": 1, "profile.content_settings.exceptions.plugins.*,*.per_resource.adobe-flash-player": 1, "PluginsAllowedForUrls": "http://abc.go.com/watch-live"})
-		options.add_argument('--always-authorize-plugins')
-		options.add_argument("--no-sandbox")
-		options.add_argument('--allow-outdated-plugins')
-		options.add_argument('--ppapi-flash-path=/usr/lib/pepperflashplugin-nonfree/libpepflashplayer.so')
+        options = webdriver.ChromeOptions()
+        options.binary_location = "/usr/bin/google-chrome-beta"
+        options.add_experimental_option( "prefs", {'plugin.state.flash': 1, "profile.default_content_setting_values.plugins":1, "profile.content_settings.plugin_whitelist.adobe-flash-player": 1, "profile.content_settings.exceptions.plugins.*,*.per_resource.adobe-flash-player": 1, "PluginsAllowedForUrls": "http://abc.go.com/watch-live"})
+        options.add_argument('--always-authorize-plugins')
+        options.add_argument("--no-sandbox")
+        options.add_argument('--allow-outdated-plugins')
+        options.add_argument('--ppapi-flash-path=/usr/lib/pepperflashplugin-nonfree/libpepflashplayer.so')
         self.driver = webdriver.Chrome(chrome_options=options)
 
     def test_page(self):
